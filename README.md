fd1d60d36ca8862485081dfd1387d68f72148448
DPattonINIT <DlamarPatton@gmail.com>
2024-11-25 14:23:07 -0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # JS-DOM-Assignment
+
+ 
+
+
+
+
---
+++ Scripts/app.js
@@ -0,0 +1,9 @@
+
+
+// ID Section
+let cbtn = document.getElementById("cbtn");
+let rbtn = document.getElementById("rbtn");
+
+let bInput = document.getElementById("bInput");
+let cInput = document.getElementById("cInput");
+
